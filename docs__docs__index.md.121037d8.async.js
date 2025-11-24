ae54817615c01fd26281bad69e044f8d21a06fca
xuzhanyi <806913282@qq.com>
2025-11-24 19:20:15 +0800

    Updates

---
+++ docs__docs__index.md.121037d8.async.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunkxzy_materials=self.webpackChunkxzy_materials||[]).push([[799],{32907:function(d,o,e){e.r(o);var s=e(6897),u=e(50018),i=e(2458),x=e(50873),c=e(31259),t=e(16682),p=e(77401),h=e(48055),a=e(6e3),l=e(10702),r=e(75271),n=e(47978),_=e(52676);function m(){return(0,_.jsx)(a.dY,{children:(0,_.jsx)(r.Suspense,{fallback:(0,_.jsx)(l.Z,{}),children:(0,_.jsx)(_.Fragment,{children:(0,_.jsxs)("div",{className:"markdown",children:[(0,_.jsxs)("h2",{id:"materialcomponents",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#materialcomponents",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"MaterialComponents"]}),(0,_.jsx)("p",{children:n.texts[0].value}),(0,_.jsxs)("h2",{id:"\u5B89\u88C5",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u5B89\u88C5"]}),(0,_.jsx)("p",{children:n.texts[1].value}),(0,_.jsx)(t.Z,{lang:"shell",children:n.texts[2].value}),(0,_.jsx)("p",{children:n.texts[3].value}),(0,_.jsx)("ul",{children:(0,_.jsx)("li",{children:(0,_.jsx)("code",{children:n.texts[4].value})})}),(0,_.jsxs)("h2",{id:"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528",children:[(0,_.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5728\u9879\u76EE\u4E2D\u4F7F\u7528",children:(0,_.jsx)("span",{className:"icon icon-link"})}),"\u5728\u9879\u76EE\u4E2D\u4F7F\u7528"]}),(0,_.jsx)("p",{children:n.texts[5].value}),(0,_.jsx)(t.Z,{lang:"tsx",children:n.texts[6].value})]})})})})}o.default=m},47978:function(d,o,e){e.r(o),e.d(o,{texts:function(){return s}});const s=[{value:"MaterialComponents \u662F\u57FA\u4E8E Ant Design \u800C\u5F00\u53D1\u7684\u7269\u6599\u7EC4\u4EF6\u3002\u8FD9\u91CC\u540E\u9762\u6DFB\u52A0",paraId:0,tocIndex:0},{value:"\u5F53\u524D MaterialComponents \u6BCF\u4E00\u4E2A\u7EC4\u4EF6\u90FD\u662F\u4E00\u4E2A\u72EC\u7ACB\u7684\u5305\uFF0C\u4F60\u9700\u8981\u5728\u4F60\u7684\u9879\u76EE\u4E2D\u5B89\u88C5\u5BF9\u5E94\u7684 npm \u5305\u5E76\u4F7F\u7528\u3002",paraId:1,tocIndex:1},{value:`$ pnpm i @xzy/xzy-materials --save
+`,paraId:2,tocIndex:1},{value:"\u5F53\u524D MaterialComponents \u63D0\u4F9B\u4E86\u5982\u4E0B\u7EC4\u4EF6\u53EF\u76F4\u63A5\u4F7F\u7528\uFF1A",paraId:3,tocIndex:1},{value:"npm i @xzy/xzy-materials --save",paraId:4,tocIndex:1},{value:"\u6BCF\u4E00\u4E2A\u5305\u90FD\u662F\u4E00\u4E2A\u72EC\u7ACB\u7684\u7EC4\u4EF6\u5305\uFF0C\u4F7F\u7528\u793A\u4F8B\u5982\u4E0B \uFF1A",paraId:5,tocIndex:2},{value:`import React from "react";
+import { ProForm, ProFormText } from "@xdf/xzy-materials";
+
+export default () => {
+  return (
+    <ProForm
+      onFinish={async (values) => {
+        console.log(values);
+      }}
+    >
+      <ProFormText name="name" label="\u59D3\u540D" />
+    </ProForm>
+  );
+};
+`,paraId:6,tocIndex:2}]}}]);
