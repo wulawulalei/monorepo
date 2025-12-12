92353c2f336916456e0d7740d06a7f4af504563d
xuzhanyi <806913282@qq.com>
2025-12-12 15:33:40 +0800

    feat: 打包配置调整

--- father.config.ts
+++ father.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'father';
+import { defineConfig } from "father";
 
-const ignores = ['src/demos/**/*', '**/*.md'];
+const ignores = ["src/demos/**/*", "**/*.md"];
 
 export default defineConfig({
   esm: {
     ignores,
-    input: 'src', // 默认编译目录
-    output: 'es',
-    platform: 'browser', // 默认构建为 Browser 环境的产物
-    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+    input: "src", // 默认编译目录
+    output: "es",
+    platform: "browser", // 默认构建为 Browser 环境的产物
+    transformer: "babel", // 默认使用 babel 以提供更好的兼容性
   },
   // 以下为 cjs 配置项启用时的默认值，有自定义需求时才需配置
   cjs: {
     ignores,
-    input: 'src', // 默认编译目录
-    output: 'lib',
-    platform: 'browser', // 默认构建为 Node.js 环境的产物
-    transformer: 'babel', // 默认使用 esbuild 以获得更快的构建速度
+    input: "src", // 默认编译目录
+    output: "lib",
+    platform: "browser", // 默认构建为 Node.js 环境的产物
+    transformer: "babel", // 默认使用 esbuild 以获得更快的构建速度
   },
 });
